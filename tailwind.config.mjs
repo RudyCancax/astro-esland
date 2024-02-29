aad02156e9faad73db3c9991e8eff3d5b078bd8c
rudycancax <44583578+RudyCancax@users.noreply.github.com>
2024-02-28 18:00:22 -0600

    Feat: 2 sections

--- tailwind.config.mjs
+++ tailwind.config.mjs
@@ -1,8 +1,16 @@
+import animations from "tailwindcss-animated";
 /** @type {import('tailwindcss').Config} */
+
+
 export default {
 	content: ['./src/**/*.{astro,html,js,jsx,md,mdx,svelte,ts,tsx,vue}'],
 	theme: {
-		extend: {},
+		extend: {
+			colors : {
+				'main' : "#0d1332",
+				'white-80' : 'rgba(255, 255, 255, 0.8)'
+			}
+		},
 	},
-	plugins: [],
+	plugins: [animations],
 }
